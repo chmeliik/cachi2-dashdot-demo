1cb7a5967721322f4addcbb142f74e335c3008fb
Adam Cmiel <acmiel@redhat.com>
2023-01-10 13:17:44 +0100

    Fix cachi2 invocation
    After the recent changes, the --package parameter no longer exists.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- prefetch.sh
+++ prefetch.sh
@@ -3,6 +3,6 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 DIR=${1:-'.'}
 
-cachi2 fetch-deps --package pip --source "$DIR" --output /tmp/cachi2-output
+cachi2 fetch-deps pip --source "$DIR" --output /tmp/cachi2-output
 cachi2 generate-env /tmp/cachi2-output --for-output-dir /cachi2 -o /tmp/cachi2-output/cachi2.env
 cachi2 inject-files /tmp/cachi2-output --for-output-dir /cachi2
